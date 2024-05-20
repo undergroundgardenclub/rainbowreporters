6ebdac999c68f8946bdafb17f6af49e6cd5c3ee2
Mark <x@markthemark.com>
2024-05-19 22:00:11 -0400

    routes: /proteins + /proteins/pdb/id

--- services/backends-api/src/start.py
+++ services/backends-api/src/start.py
@@ -1,20 +1,29 @@
 from fastapi import FastAPI
+from fastapi.responses import FileResponse, ORJSONResponse
 import os
-import json
+import pandas as pd
 import uvicorn
-import os
 
-app = FastAPI()
+app = FastAPI(default_response_class=ORJSONResponse) # 'NaN' in the dataset throws errs in fastapi response
+data_dir = os.path.join(os.path.dirname(__file__), "..", "data")
 
-@app.get("/")
-def read_root():
-  return "howdy"
 
-@app.get("/data/listdir")
-def read_json():
-  data_dir = os.path.join(os.path.dirname(__file__), "..", "data")
-  data_files = os.listdir(data_dir)
-  return data_files
+# ROUTES
+@app.get("/proteins")
+async def get_proteins():
+    fps_df = pd.read_csv(f"{data_dir}/fluorescent_proteins.csv")
+    fps_df = fps_df[fps_df['pdb.0'].notnull()] # Filter rows with non-null 'pdb.0' values
+    fps_dict = fps_df[['uuid', 'pdb.0', 'seq', 'slug', 'states.0.brightness', 'states.0.em_max', 'states.0.ex_max']].rename(columns={"states.0.brightness": "brightness", "states.0.em_max": "em_max", "states.0.ex_max": "ex_max"}).to_dict(orient="records")
+    return fps_dict
 
+@app.get("/proteins/pdb/{pdb_id}")
+async def get_protein_pdb(pdb_id: str):
+    pdb_dir = f"{data_dir}/pdbs"
+    pdb_file = f"{pdb_dir}/{pdb_id}.pdb"
+    # send file from local dir
+    return FileResponse(pdb_file)
+
+
+# SERVE
 if __name__ == "__main__":
-  uvicorn.run(app, host="0.0.0.0", port=3000)
+    uvicorn.run(app, host="0.0.0.0", port=3000)
